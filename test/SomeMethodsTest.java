a100e603290e7f598b0f1bb33e5e5479c62c1f3e
Hanh Doan <s3639869@rmit.edu.vn>
2021-07-21 15:37:33 +0700

    Modify unit test module

--- test/SomeMethodsTest.java
+++ test/SomeMethodsTest.java
@@ -10,6 +10,10 @@ class SomeMethodsTest {
     }
 
     @Test
-    void weight_index() {
+    public void testIndexValCalculation() {
+        assertEquals(0, SomeMethods.weight_index(0,10));
+        assertEquals(1,SomeMethods.weight_index(1,1));
+        assertEquals(2,SomeMethods.weight_index(8,2));
+        assertEquals(3,SomeMethods.weight_index(3,1));
     }
 }
